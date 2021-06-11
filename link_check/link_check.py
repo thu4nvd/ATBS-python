19c935c87fa54e3f778b02b0908eb70c6b019b16
VU DUC Thuan <thuanvd@outlook.com>
2021-06-11 09:06:15 +0700

    [update] structure of files and scripts

---
+++ link_check/link_check.py
@@ -0,0 +1,22 @@
+
+# Author: bnmd
+# Structure of input Excel file: 	url,username,password,Domain,Expect,Result-check
+# Script read the content of Excel file and do check the link.
+# Version 1: check the link, without the credentials.
+
+
+
+
+# Read excel file
+# for each line
+# 	check url()
+# 		if domain()
+# 		if username+password
+# 		result = return()
+# 	write result to the excel file.
+# 	send email.
+
+# Schedule to run the script.
+# Write log file.
+
+# schedule the send email.
---
+++ urlchecker/README.md
@@ -0,0 +1,6 @@
+<!-- download and install url checker -->
+py -3 -m venv .venv
+.\.venv\Scripts\activate
+python -m pip install urlchecker
+
+Note: have to connect VPN before checking the links in taipc.txt ???
